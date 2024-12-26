1d9d7d04f71f337216cfae5273a85ee95caa37b7
Spidey <spiderLambo.dev@outlook.com>
2024-12-26 16:27:51 +0100

    vars interpreter

---
+++ errors.py
@@ -0,0 +1,3 @@
+def raise_error(error):
+    print(f"\033[0;31m{error}")
+    exit()
---
+++ interpreter.py
@@ -0,0 +1,49 @@
+from errors import *
+
+
+with open("file.couleuvre", "r") as doc:
+    document = doc.readlines()
+    for i in range(len(document)):
+        if document[i][-1:] == "\n":
+            document[i] = document[i][:-1]
+
+
+vars = {}
+for line in document:
+    for i in range(len(line)):
+        # Equality
+        if line[i] == "=":
+            # Before equality verification
+            if i != 0:
+                if line[i-1] == " ":
+                    if i - 1 == 0:
+                        raise_error("plz put somthing before the =")
+                    else:
+                        vars[line[:i-1]] = None
+                else:
+                    vars[line[:i]] = None
+            elif i == 0:
+                raise_error("plz put somthing before the =")
+
+            # After equality verification
+            if i+1 <= len(line)-1:
+                if line[i+1] == "=":
+                    raise_error("plz don't enter a comparaison operator alone")
+                elif line[i+1] == " ":
+                    if i+1 == len(line)-1:
+                        raise_error("plz put somthing after the =")
+                    else:
+                        if line[:i] in vars:
+                            vars[line[:i]] = line[i+2:]
+                        else:
+                            vars[line[:i-1]] = line[i + 2:]
+                else:
+                    if line[:i] in vars:
+                        vars[line[:i]] = line[i + 1:]
+                    else:
+                        vars[line[:i - 1]] = line[i + 1:]
+            else:
+                raise_error("plz put somthing after the =")
+
+
+print(vars)
